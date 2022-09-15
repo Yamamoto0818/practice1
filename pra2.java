46e05dabaaadc54812288d2e15a03a2586512151
Yamamoto <m.yamamoto.sys22@morijyobi.ac.jp>
2022-09-15 12:49:41 +0900

    pra2.javaに出力処理追記
    hello Worldを１００回出力するプログラム

--- pra2.java
+++ pra2.java
@@ -1,8 +1,8 @@
 public class pra2 {
 
 	public static void main(String args[]){
-		for (int i = 0;i < 100 ; i++){
-		System.out.plintln("Hello World");
+		for (int i = 0;i < 101 ; i++){
+		System.out.println("Hello World");
 		}
 	}
 }
